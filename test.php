ff0677d06fdeffeaa0f302cd8466802064d24ef2
miguellara <m_lara_r@hotmail.com>
2015-01-19 17:23:24 +0000

    third change

--- test.php
+++ test.php
@@ -3,7 +3,10 @@
 echo "It seems it's working!";
 echo "change from codio 2";
 echo "change from codio";
+echo "change from codio 3";
 /*
  * To change this template use Tools | Templates.
+ */
+?>se Tools | Templates.
  */
 ?>
